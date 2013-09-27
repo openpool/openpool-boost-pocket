bf8ec0c01311d77b571e148beb2c218a94056492
kakenman <s.shinya5225@gmail.com>
2013-09-27 02:45:18 -0400

    Added 1st version of the pocket detector interface

---
+++ osc-module/Pocket_detector_interface/Pocket_detector_interface/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace Pocket_detector_interface
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.output_box = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(60, 197);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // output_box
+            // 
+            this.output_box.Location = new System.Drawing.Point(35, 35);
+            this.output_box.Multiline = true;
+            this.output_box.Name = "output_box";
+            this.output_box.Size = new System.Drawing.Size(196, 144);
+            this.output_box.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(278, 244);
+            this.Controls.Add(this.output_box);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox output_box;
+    }
+}
+
---
+++ osc-module/Pocket_detector_interface/Pocket_detector_interface/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MakingThings;
+using System.IO.Ports;
+using System.Threading;
+
+namespace Pocket_detector_interface
+{
+    public partial class Form1 : Form
+    {
+        private UdpPacket udpPacket;
+        private Osc oscUdp;
+        OscMessage oscMsg;
+        SerialPort myPort;   
+        public Form1()
+        {
+            udpPacket = new UdpPacket();
+            udpPacket.RemoteHostName = "127.0.0.1";
+            udpPacket.RemotePort = 7000;
+            udpPacket.LocalPort = 9000;
+            udpPacket.Open();
+            oscUdp = new Osc(udpPacket);
+
+            string PortName = "COM8";
+            int BaudRate = 115200;
+            Parity Parity = Parity.None;
+            int DataBits = 8;
+            StopBits StopBits = StopBits.One;
+            
+            myPort = new SerialPort(PortName, BaudRate, Parity, DataBits, StopBits);
+            myPort.Open();
+            byte[] buf = new byte[] { (byte)'s' };
+            myPort.Write(buf, 0, buf.Length);
+
+
+            InitializeComponent();
+            backgroundWorker1.RunWorkerAsync();
+
+        }
+
+        private byte[] read_serial_port()
+        {
+            int rbyte = myPort.BytesToRead;
+            byte[] buffer = new byte[rbyte];
+            int read = 0;
+            while (read < rbyte)
+            {
+                int length = myPort.Read(buffer, read, rbyte - read);
+                read += length;
+            }
+            return buffer;
+        }
+
+        delegate void add_text_delegate(String str);
+
+        void add_text(String str)
+        {
+            output_box.Text = str + Environment.NewLine + output_box.Text;
+        }
+
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (true)
+            {
+                byte[] buf = new byte[] { (byte)'r' };
+                myPort.Write(buf, 0, buf.Length);
+                Thread.Sleep(2000);
+                buf = read_serial_port();
+                String serial_str = Encoding.ASCII.GetString(buf);
+                Invoke(new add_text_delegate(add_text),serial_str);
+                String[] fallcounts = serial_str.Trim().Split(',');
+                String osc_message_str = "/pocket";
+                int i;
+                osc_message_str += " " + fallcounts.Length.ToString();
+                for (i = 0; i < 6 && i < fallcounts.Length; i++)
+                {
+                    osc_message_str += " " + fallcounts[i];
+                }
+
+                oscMsg = Osc.StringToOscMessage(osc_message_str);
+                oscUdp.Send(oscMsg);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("send data");
+            //パスはアプリの仕様で適当に、今回はウインドウの幅を送ります。
+            oscMsg = Osc.StringToOscMessage("/event/click " + Width.ToString());
+            oscUdp.Send(oscMsg);
+//            byte[] buf = new byte[] { (byte)'t', (byte)'s', (byte)'\r', (byte)'\n' };
+
+
+        }
+    }
+}
